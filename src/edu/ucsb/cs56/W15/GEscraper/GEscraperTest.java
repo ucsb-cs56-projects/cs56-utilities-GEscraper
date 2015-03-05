94b6d133f98634c8b4a7a169257f2575502f90ca
Dylan Lynch <dlynch@umail.ucsb.edu>
2015-03-04 20:45:12 -0800

    Added getSpecificCourse which takes Area and Subject arguments and returns a list with all courses that fulfill both arguments.

--- src/edu/ucsb/cs56/W15/GEscraper/GEscraperTest.java
+++ src/edu/ucsb/cs56/W15/GEscraper/GEscraperTest.java
@@ -9,39 +9,53 @@ import java.net.*;
 /** test class for GEscraper
 
 @author Brian Wan
-@version 2013.06.07
+@author Dylan Lynch
+@version 03.04.15
 @see GEscraper
 
 */
 
 public class GEscraperTest {
 
-    /** Test getCourses with General Course Input
+    /** Test getSpecificCourses with General Course Input
     @see GEscraper
     */
     @Test
-    public void test_getCoursesGeneral(){
+    public void test_getSpecificCourses() {
       GEscraper g = new GEscraper();
-      ArrayList<String> s = g.getCourses("B");
-
-      String a = s.get(0);
-      String b = "         ANTH      2   Introductory Cultural Anthropology";
-
-      assertEquals(a, b);
+      // ArrayList with only Area B French courses
+      ArrayList<String> f = g.getSpecificCourses("B", "FR");
+      // ArrayList with all Area B Courses
+      ArrayList<String> a = g.getCourses("B");
+      System.out.println("getCourses:         " + a.get(7));
+      System.out.println("getSpecificCourses: " + f.get(0));
+      assertEquals(f.get(0), a.get(7));
     }
 
-    /** Test getCourses with Special Course Subject Input"
-    @see GEscraper
-    */
-    @Test
-    public void test_getCoursesSpecial(){
-      GEscraper g = new GEscraper();
-      ArrayList<String> s = g.getCourses("QNT");
+    // /** Test getCourses with General Course Input
+    // @see GEscraper
+    // */
+    // @Test
+    // public void test_getCoursesGeneral(){
+    //   GEscraper g = new GEscraper();
+    //   ArrayList<String> s = g.getCourses("B");
 
-      String a = s.get(0);
-      String b = "         ASTRO     1   Basic Astronomy";
+    //   String a = s.get(0);
+    //   assertEquals(a, b);
+    // }
 
-      assertEquals(a, b);
-    }
+    // * Test getCourses with Special Course Subject Input"
+    // @see GEscraper
+    
+    // @Test
+    // public void test_getCoursesSpecial(){
+    //   GEscraper g = new GEscraper();
+    //   ArrayList<String> s = g.getCourses("QNT");
+
+    //   String a = s.get(0);
+    //   String b = "         ASTRO     1   Basic Astronomy";
+
+    //   assertEquals(a, b);
+    // }
     
 }
