bc47c91765dd5ff1db9a16651909d500343414fe
Xingyuan <lin1995@umail.ucsb.edu>
2016-11-20 15:35:29 -0800

    delete comments

--- src/edu/ucsb/cs56/F16/CoursesScraper/GetDescription.java
+++ src/edu/ucsb/cs56/F16/CoursesScraper/GetDescription.java
@@ -47,38 +47,6 @@ public class GetDescription {
 	}
 
 	public static String getEngCourseDescription(String course) {
-
-		// String[] words = course.split(" - ");
-		// String[] words2 = words[0].split(" [0-9]+");
-		// String area = words2[0];
-		// String courseId = words[0] + ".";
-		// AreaUrlMappingTable table = new AreaUrlMappingTable();
-		// String url = table.getUrl(area);
-		// Document doc=Jsoup.parse("");
-		// try{
-		// 	doc = Jsoup.connect(url).get();
-		// }
-		// //Catch bad URL
-		// catch (MalformedURLException e) {
-		// 	System.out.println("Area does not exist.");
-		// 	return "Course description not found";
-		// }
-		// //catch IOException
-		// catch (IOException e) {
-		// 	System.out.println("Check Internet.");
-		// 	return "Course description not found";
-		// }
-		// Elements content = doc.getElementsContainingText(courseId);
-		// if(content.size() == 0)
-		// 	return "Course description not found";
-		// else{
-		// 	Element panel = content.get(content.size() - 2);
-		// 	Elements des = panel.select("div[style=padding-left: 2px;]");
-		// 	if(des.size() == 0)
-		// 		return "Course description not found";
-		// 	else
-		// 		return des.get(1).html();			
-		// }
 		String[] words = course.split("\\. ");
 		String[] words2 = words[0].split(" [0-9]+");
 		String area = words2[0];
