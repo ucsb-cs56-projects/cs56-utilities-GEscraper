b8990ce4233a285a34a5877e1fecab3c386a2935
Ekta Shahani <shahani.ekta@gmail.com>
2017-11-08 16:49:04 -0800

    created file for start page

---
+++ ScraperGUI.java
@@ -0,0 +1,15 @@
+package edu.ucsb.cs56.F16.CourseScraper;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class ScraperGUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    
+    
+}
