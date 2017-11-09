bf95756382df788484f0e6f13ead49c4d9ee66ff
Ekta Shahani <shahani.ekta@gmail.com>
2017-11-08 22:46:24 -0800

    Remade start page

--- src/edu/ucsb/cs56/F16/CoursesScraper/CourseScraper.java
+++ src/edu/ucsb/cs56/F16/CoursesScraper/CourseScraper.java
@@ -18,110 +18,112 @@ import java.util.Scanner;
 public class CourseScraper {
 	//main	
 	public static void main(String args[]){
-		boolean loop = true;
-		ArrayList<String> list = new ArrayList<String>();
-		String area, department;
-		GetGeInfo getgeinfo = new GetGeInfo();
-		while (loop == true) {
-
-		    Scanner areaScanner = new Scanner(System.in);
-		    Scanner departmentScanner = new Scanner(System.in);
-		    
-		    Object[] options = {"Engineering",
-                    "General Subject"};
-			int engrOrGe = JOptionPane.showOptionDialog(null,
-			    "Choose to display courses from engineering or general subject",
-			    "Menu",
-			    JOptionPane.YES_NO_OPTION,
-			    JOptionPane.QUESTION_MESSAGE,
-			    null,
-			    options,
-			    options[0]);
-			if(engrOrGe==JOptionPane.YES_OPTION){
-			    String[] departments={"Chemical Engineering","Computer Engineering", "Computer Science - Engineering", "Electrical and Computer Engineering", "Engineering Sciences", "Materials", "Mechanical Engineering", "Technology Management Program"};
-			    department = (String) JOptionPane.showInputDialog(null, "Enter a Department Name:", "Menu", JOptionPane.QUESTION_MESSAGE, null, departments, departments[0]);
-				String dept = "";
-				switch(department){
-					case "Chemical Engineering": dept = "chemengr";break;
-					case "Computer Engineering": dept = "compengr";break;
-					case "Computer Science - Engineering": dept = "compsci-engr";break;
-					case "Electrical and Computer Engineering": dept = "ece";break;
-					case "Engineering Sciences": dept = "engrsci";break;
-					case "Materials": dept = "matrl";break;
-					case "Mechanical Engineering": dept = "mechengr";break;
-					case "Technology Management Program": dept = "tmp";break;
-				}
-				ArrayList<String> courses = GetEngInfo.getCourses(dept);
-				
-				String[] data = courses.toArray(new String[courses.size()]);
-			    javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            		public void run() {
-                		GetEngInfoGUI.createAndShowGUI(data);
-            		}
-        		});
-        		
-			    System.out.println("Scrape? Y/N");
-			    area = areaScanner.next();
-			    if (area.equals("N")) { loop = false; }
-			}
-			else if(engrOrGe==JOptionPane.NO_OPTION){
-				//System.out.println("Enter a Subject Area (B-H or WRT, EUR, NWC, QNT, ETH) or enter HELP for a list of all areas and courses");
-			    //area = areaScanner.next();
-			    String[] choices={"B", "C", "D", "E", "F", "G", "H", "WRT", "EUR", "QNT", "NWC", "ETH"};
-			    // JOptionPane: pop up a standard dialog box
-			    area = (String) JOptionPane.showInputDialog(null, "Enter a Subject Area:", "Menu", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
-			    System.out.println(area);
-			    
-			    if (area.equals("HELP")) {
-					System.out.println("General/Special Subject Area Inputs:");
-					System.out.println("Input = Course List Result");
-					System.out.println("Area B Courses = B");
-					System.out.println("Area C Courses = C");
-					System.out.println("Area D Courses = D");
-					System.out.println("Area E Courses = E");
-					System.out.println("Area F Courses = F");
-					System.out.println("Area G Courses = G");
-					System.out.println("Area H Courses = H");
-					System.out.println("Writing Courses = WRT");
-					System.out.println("European Courses = EUR");
-					System.out.println("World Cultures Courses = NWC");
-					System.out.println("Quantitative Courses = QNT");
-					System.out.println("Ethnicity Courses = ETH");
-					
-					ArrayList<ArrayList<String>> departments = getgeinfo.getDepartments();
-					ArrayList<String> departmentt;
-					for (int i = 0; i < departments.size(); i++) {
-					    departmentt = departments.get(i);
-					    System.out.println(departmentt.get(0) + " = " + departmentt.get(1));
-					}
-					//System.out.println("Enter a Subject Area: ");
-					//area = areaScanner.next();
-				}
-			    
-			    //System.out.println("Enter a specific department abbrevation or N (No): ");  
-			    department = (String) JOptionPane.showInputDialog("Enter a specific department abbrevation or N (No): ");
-				//department = departmentScanner.next();    
-				if (department.equals("N")) {
-					list = getgeinfo.getCourses(area);
-				}
-				else { 
-					list = getgeinfo.getSpecificCourses(area, department);
-			    }
-			    
-			    String[] data = list.toArray(new String[list.size()]);
-
-
-               	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            		public void run() {
-                		GetGeInfoGUI.createAndShowGUI(data);
-            		}
-        		});
-            	
-			    System.out.println("Scrape? Y/N");
-			    area = areaScanner.next();
-			    if (area.equals("N")) { loop = false; }
-			}
-		}
+		
+		ScraperGUI startPage = new ScraperGUI();
+		// boolean loop = true;
+		// ArrayList<String> list = new ArrayList<String>();
+		// String area, department;
+		// GetGeInfo getgeinfo = new GetGeInfo();
+		// while (loop == true) {
+    // 
+		//     Scanner areaScanner = new Scanner(System.in);
+		//     Scanner departmentScanner = new Scanner(System.in);
+    // 
+		//     Object[] options = {"Engineering",
+    //                 "General Subject"};
+		// 	int engrOrGe = JOptionPane.showOptionDialog(null,
+		// 	    "Choose to display courses from engineering or general subject",
+		// 	    "Menu",
+		// 	    JOptionPane.YES_NO_OPTION,
+		// 	    JOptionPane.QUESTION_MESSAGE,
+		// 	    null,
+		// 	    options,
+		// 	    options[0]);
+		// 	if(engrOrGe==JOptionPane.YES_OPTION){
+		// 	    String[] departments={"Chemical Engineering","Computer Engineering", "Computer Science - Engineering", "Electrical and Computer Engineering", "Engineering Sciences", "Materials", "Mechanical Engineering", "Technology Management Program"};
+		// 	    department = (String) JOptionPane.showInputDialog(null, "Enter a Department Name:", "Menu", JOptionPane.QUESTION_MESSAGE, null, departments, departments[0]);
+		// 		String dept = "";
+		// 		switch(department){
+		// 			case "Chemical Engineering": dept = "chemengr";break;
+		// 			case "Computer Engineering": dept = "compengr";break;
+		// 			case "Computer Science - Engineering": dept = "compsci-engr";break;
+		// 			case "Electrical and Computer Engineering": dept = "ece";break;
+		// 			case "Engineering Sciences": dept = "engrsci";break;
+		// 			case "Materials": dept = "matrl";break;
+		// 			case "Mechanical Engineering": dept = "mechengr";break;
+		// 			case "Technology Management Program": dept = "tmp";break;
+		// 		}
+		// 		ArrayList<String> courses = GetEngInfo.getCourses(dept);
+    // 
+		// 		String[] data = courses.toArray(new String[courses.size()]);
+		// 	    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+    //         		public void run() {
+    //             		GetEngInfoGUI.createAndShowGUI(data);
+    //         		}
+    //     		});
+    // 
+		// 	    System.out.println("Scrape? Y/N");
+		// 	    area = areaScanner.next();
+		// 	    if (area.equals("N")) { loop = false; }
+		// 	}
+		// 	else if(engrOrGe==JOptionPane.NO_OPTION){
+		// 		//System.out.println("Enter a Subject Area (B-H or WRT, EUR, NWC, QNT, ETH) or enter HELP for a list of all areas and courses");
+		// 	    //area = areaScanner.next();
+		// 	    String[] choices={"B", "C", "D", "E", "F", "G", "H", "WRT", "EUR", "QNT", "NWC", "ETH"};
+		// 	    // JOptionPane: pop up a standard dialog box
+		// 	    area = (String) JOptionPane.showInputDialog(null, "Enter a Subject Area:", "Menu", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+		// 	    System.out.println(area);
+    // 
+		// 	    if (area.equals("HELP")) {
+		// 			System.out.println("General/Special Subject Area Inputs:");
+		// 			System.out.println("Input = Course List Result");
+		// 			System.out.println("Area B Courses = B");
+		// 			System.out.println("Area C Courses = C");
+		// 			System.out.println("Area D Courses = D");
+		// 			System.out.println("Area E Courses = E");
+		// 			System.out.println("Area F Courses = F");
+		// 			System.out.println("Area G Courses = G");
+		// 			System.out.println("Area H Courses = H");
+		// 			System.out.println("Writing Courses = WRT");
+		// 			System.out.println("European Courses = EUR");
+		// 			System.out.println("World Cultures Courses = NWC");
+		// 			System.out.println("Quantitative Courses = QNT");
+		// 			System.out.println("Ethnicity Courses = ETH");
+    // 
+		// 			ArrayList<ArrayList<String>> departments = getgeinfo.getDepartments();
+		// 			ArrayList<String> departmentt;
+		// 			for (int i = 0; i < departments.size(); i++) {
+		// 			    departmentt = departments.get(i);
+		// 			    System.out.println(departmentt.get(0) + " = " + departmentt.get(1));
+		// 			}
+		// 			//System.out.println("Enter a Subject Area: ");
+		// 			//area = areaScanner.next();
+		// 		}
+    // 
+		// 	    //System.out.println("Enter a specific department abbrevation or N (No): ");  
+		// 	    department = (String) JOptionPane.showInputDialog("Enter a specific department abbrevation or N (No): ");
+		// 		//department = departmentScanner.next();    
+		// 		if (department.equals("N")) {
+		// 			list = getgeinfo.getCourses(area);
+		// 		}
+		// 		else { 
+		// 			list = getgeinfo.getSpecificCourses(area, department);
+		// 	    }
+    // 
+		// 	    String[] data = list.toArray(new String[list.size()]);
+    // 
+    // 
+    //            	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+    //         		public void run() {
+    //             		GetGeInfoGUI.createAndShowGUI(data);
+    //         		}
+    //     		});
+    // 
+		// 	    System.out.println("Scrape? Y/N");
+		// 	    area = areaScanner.next();
+		// 	    if (area.equals("N")) { loop = false; }
+		// 	}
+		// }
 	}//end main
     
 }//end GEscraper
---
+++ src/edu/ucsb/cs56/F16/CoursesScraper/ScraperGUI.java
@@ -0,0 +1,56 @@
+package edu.ucsb.cs56.F16.CourseScraper;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.Component;
+
+import javax.swing.*;
+
+public class ScraperGUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    
+    public ScraperGUI() {
+      this.setTitle("UCSB Course Finder");
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+      
+      JPanel buttonPane = new JPanel();
+      buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
+      
+      JLabel choices = new JLabel("Choose which area to display classes from");
+      choices.setAlignmentX(Component.CENTER_ALIGNMENT);
+      
+      JButton engBtn = new JButton("Engineering");
+      engBtn.addActionListener(new EngListener());
+      JButton genBtn = new JButton("General Education");
+      genBtn.addActionListener(new GenListener());
+      
+      buttonPane.add(genBtn);
+      buttonPane.add(Box.createRigidArea(new Dimension(20, 0)));
+      buttonPane.add(engBtn);
+      
+      add(Box.createRigidArea(new Dimension(0, 230)));
+      add(choices);
+      add(Box.createRigidArea(new Dimension(0, 20)));
+      add(buttonPane);
+      
+      this.setSize(600, 600);
+      this.setVisible(true);
+    }
+      
+    private class EngListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+        getContentPane().removeAll();
+        getContentPane().repaint();
+      }
+    }
+    
+    private class GenListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+        getContentPane().removeAll();
+        getContentPane().repaint();
+      }
+    }
+}
