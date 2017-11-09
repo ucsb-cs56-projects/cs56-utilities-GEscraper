04ad81f13813150e4ed6000c3fb3cd41189d9e41
aditya-nadkarni <nadkarniaditya@gmail.com>
2017-11-09 00:33:08 -0800

    Updated GUI and Multiple Searches for GE

--- src/edu/ucsb/cs56/F16/CoursesScraper/GeGUI.java
+++ src/edu/ucsb/cs56/F16/CoursesScraper/GeGUI.java
@@ -13,102 +13,100 @@ public class GeGUI extends JPanel{
   private JScrollPane courseList;
   private JTextArea text;
   private JComboBox subjectDropdown;
+  private JComboBox departmentDropdown;
   private JButton getDescriptionButton;
+  private AreaUrlMappingTable tb;
 
   public GeGUI(){
     //Arrays for dropdown menus
     String[] choices = {"B", "C", "D", "E", "F", "G", "H", "WRT", "EUR", "QNT", "NWC", "ETH"};
-    AreaUrlMappingTable tb = new AreaUrlMappingTable();
-    Map areas = tb.getGEAreas();                      
-    String[] departments = new String[areas.size()];
+    tb = new AreaUrlMappingTable();
+    Map areas = tb.getGEAreas();
+    String[] departments = new String[areas.size()+1];
+    departments[0] = "None";
     Iterator it = areas.entrySet().iterator();
-    int i = 0;
+    int i = 1;
     while (it.hasNext()) {
         Map.Entry pair = (Map.Entry)it.next();
         System.out.println((String) pair.getKey() + " = " + (String) pair.getValue());
         departments[i] = (String) pair.getKey();
-        it.remove(); 
+        it.remove();
         i++;
     }
-    subjectDropdown = new JComboBox(choices);
-  }
 
+    JLabel subjectLabel = new JLabel("Select Subject Area: ");
+    subjectDropdown = new JComboBox(choices);
+    JLabel departmentLabel = new JLabel("Select Department: ");
+    departmentDropdown = new JComboBox(departments);
     /************************
     Copy pasted from EngGUI. Was gonna change for GeGUI as I go through
     ************************/
-  // 
-  //   JButton searchButton = new JButton("SEARCH");
-  //   searchButton.addActionListener(new searchButtonListener());
-  // 
-  //   // set box layout for the frame
-  //   this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-  // 
-  //   //set top part
-  //   JPanel searchPane = new JPanel();
-  //   searchPane.setLayout(new BoxLayout(searchPane, BoxLayout.LINE_AXIS));
-  //   searchPane.add(departmentDropdown);
-  //   searchPane.add(searchButton);
-  // 
-  //   //set bottom part
-  //   JPanel foundPane = new JPanel();
-  //   foundPane.setLayout(new BoxLayout(foundPane, BoxLayout.LINE_AXIS));
-  // 
-  //   text = new JTextArea("Course Description",9,25);
-  //   text.setLineWrap(true);
-  //   text.setWrapStyleWord(true);
-  //   getDescriptionButton = new JButton("Get Description");
-  //   getDescriptionButton.addActionListener(new descriptionButtonListener());
-  //   list = new JList();
-  //   courseList = new JScrollPane(list);
-  // 
-  //   foundPane.add(courseList);
-  //   foundPane.add(getDescriptionButton);
-  //   foundPane.add(new JScrollPane(text));
-  // 
-  //   //Add components to main panel
-  //   add(searchPane);
-  //   add(foundPane);
-  //   setSize(600,600);
-  // 
-  //   setVisible(true);
-  // }
-  // 
-  // class searchButtonListener implements ActionListener{
-  //   public void actionPerformed(ActionEvent event){
-  //     String selected = (String) departmentDropdown.getSelectedItem();
-  // 
-  //     String dept = "";
-  //     switch(selected){
-  //       case "Chemical Engineering": dept = "chemengr";break;
-  //       case "Computer Engineering": dept = "compengr";break;
-  //       case "Computer Science - Engineering": dept = "compsci-engr";break;
-  //       case "Electrical and Computer Engineering": dept = "ece";break;
-  //       case "Engineering Sciences": dept = "engrsci";break;
-  //       case "Materials": dept = "matrl";break;
-  //       case "Mechanical Engineering": dept = "mechengr";break;
-  //       case "Technology Management Program": dept = "tmp";break;
-  //     }
-  // 
-  //     System.out.println("Checking Courses...");
-  //     ArrayList<String> courses = GetEngInfo.getCourses(dept);
-  //     System.out.println("Found courses!");
-  //     String[] data = courses.toArray(new String[courses.size()]);
-  //     System.out.println(data[0]);
-  //     list.setListData(data);
-  // 
-  //     courseList.setViewportView(list);
-  //     text.setText("");
-  //   }
-  // }
-  // 
-  // class descriptionButtonListener implements ActionListener{
-  //   public void actionPerformed(ActionEvent event){
-  //     String s = (String) list.getSelectedValue();
-  // 
-  //     System.out.println("Value Selected: " + s);
-  //     text.setText(GetDescription.getEngCourseDescription(s));
-  //   }
-  // }
 
-  //Do scraping
+    JButton searchButton = new JButton("SEARCH");
+    searchButton.addActionListener(new searchButtonListener());
+
+    // set box layout for the frame
+    this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+    //set top part
+    JPanel searchPane = new JPanel();
+    searchPane.setLayout(new BoxLayout(searchPane, BoxLayout.Y_AXIS));
+    searchPane.add(subjectLabel);
+    searchPane.add(subjectDropdown);
+    searchPane.add(departmentLabel);
+    searchPane.add(departmentDropdown);
+    searchPane.add(searchButton);
+
+    //set bottom part
+    JPanel foundPane = new JPanel();
+    foundPane.setLayout(new BoxLayout(foundPane, BoxLayout.LINE_AXIS));
+
+    text = new JTextArea("Course Description",9,25);
+    text.setLineWrap(true);
+    text.setWrapStyleWord(true);
+    getDescriptionButton = new JButton("Get Description");
+    getDescriptionButton.addActionListener(new descriptionButtonListener());
+    list = new JList();
+    courseList = new JScrollPane(list);
+
+    foundPane.add(courseList);
+    foundPane.add(getDescriptionButton);
+    foundPane.add(new JScrollPane(text));
+
+    //Add components to main panel
+    add(searchPane);
+    add(foundPane);
+    setSize(600,600);
+
+    setVisible(true);
+  }
+
+  class searchButtonListener implements ActionListener{
+    public void actionPerformed(ActionEvent event){
+      String subject = (String) subjectDropdown.getSelectedItem();
+      String department = (String) departmentDropdown.getSelectedItem();
+      ArrayList<String> courses;
+      if(department.equals("None")){
+        courses = GetGeInfo.getCourses(subject);
+      }
+      else{
+        courses = GetGeInfo.getSpecificCourses(subject, department);
+      }
+
+      String[] data = courses.toArray(new String[courses.size()]);
+      System.out.println(data[0]);
+      list.setListData(data);
+
+      courseList.setViewportView(list);
+      text.setText("");
+    }
+  }
+
+  class descriptionButtonListener implements ActionListener{
+    public void actionPerformed(ActionEvent event){
+      String s = (String) list.getSelectedValue();
+      System.out.println("Value Selected: " + s);
+      text.setText(GetDescription.getGECourseDescription(s));
+    }
+  }
 }
--- src/edu/ucsb/cs56/F16/CoursesScraper/ScraperGUI.java
+++ src/edu/ucsb/cs56/F16/CoursesScraper/ScraperGUI.java
@@ -10,36 +10,36 @@ import javax.swing.*;
 public class ScraperGUI extends JFrame {
 
     private static final long serialVersionUID = 1L;
-    
+
     public ScraperGUI() {
       this.setTitle("UCSB Course Finder");
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-      
+
       JPanel buttonPane = new JPanel();
       buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
-      
+
       JLabel choices = new JLabel("Choose which area to display classes from");
       choices.setAlignmentX(Component.CENTER_ALIGNMENT);
-      
+
       JButton engBtn = new JButton("Engineering");
       engBtn.addActionListener(new EngListener());
       JButton genBtn = new JButton("General Education");
       genBtn.addActionListener(new GenListener());
-      
+
       buttonPane.add(genBtn);
       buttonPane.add(Box.createRigidArea(new Dimension(20, 0)));
       buttonPane.add(engBtn);
-      
+
       add(Box.createRigidArea(new Dimension(0, 130)));
       add(choices);
       add(Box.createRigidArea(new Dimension(0, 20)));
       add(buttonPane);
-      
+
       this.setSize(800, 420);
       this.setVisible(true);
     }
-      
+
     private class EngListener implements ActionListener {
       public void actionPerformed(ActionEvent e) {
         getContentPane().removeAll();
@@ -49,12 +49,13 @@ public class ScraperGUI extends JFrame {
         validate();
       }
     }
-    
+
     private class GenListener implements ActionListener {
       public void actionPerformed(ActionEvent e) {
         getContentPane().removeAll();
-        GeGUI test = new GeGUI();
+        add(new GeGUI());
         getContentPane().repaint();
+        validate();
       }
     }
 }
